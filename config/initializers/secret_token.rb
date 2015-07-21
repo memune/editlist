7e83c72dcbab40035d20f6486c46508f953c781a
memune <ordinary0826@gmail.com>
2015-07-21 16:29:22 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Editlist::Application.config.secret_key_base = 'fb5a016d7461e912cd79d6687f72e6436ec02eb946c44ee7014e76a10f88598658cb2f39e246afe5eea04b768a79f75497afd97ac85d5256c08bcc5d64769954'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Editlist::Application.config.session_store :cookie_store, key: '_editlist_session'
